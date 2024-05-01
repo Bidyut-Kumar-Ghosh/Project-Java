17e94fd9e36792a7fcf170c305ad40145b5ec14c
Bidyut <deepghosh369@gmail.com>
2024-05-01 14:12:39 +0530

    New Edit

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/**
+ * Hello
+ */
+public class Hello {
+    public static void main(String[] args) 
+    {
+        int myage;
+        String myname;
+        myage = 22;
+        myname = "Bidyut Kumar Ghosh";
+
+        
+        System.out.println(myname);
+        System.out.println(myage);
+    }
+}
+
+//New Edit
